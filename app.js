f032830f379303c06b3f6514d1830b0d31931e96
Kevin Bluer <kevin@bluer.com>
2014-04-19 22:36:19 +0800

    origin

---
+++ app.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var port = process.env.PORT || 3002;
+var app = express();
+var mongoose = require('mongoose');
+var Photo = require('./models/photo.js');
+ 
+
+app.configure('development', function(){
+	mongoose.connect('mongodb://pintrigue:p1ntr1gu3@alex.mongohq.com:10067/pintrigue');
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+	mongoose.connect('mongodb://pintrigue:p1ntr1gu3@alex.mongohq.com:10067/pintrigue');
+    app.use(express.errorHandler()); 
+});
+
+app.listen(port, function() {
+  console.log("Instagraaaam API is Listening on " + port);
+});
+
+app.get('/getPhotos', function(req, res) {
+		
+	Photo.find(function(err, photo) {
+		if (err) { throw err; }
+
+		res.send(photo);
+  	});
+});
+
+app.post('/postPhoto', function(req, res) {
+
+	photo = new Photo({
+	  title: req.body.title,
+	  content: req.body.content,
+	  url: req.body.url
+	});
+
+	photo.save(function(err) {
+		if (err) { throw err; }
+
+		res.send(photo);
+	});
+});
